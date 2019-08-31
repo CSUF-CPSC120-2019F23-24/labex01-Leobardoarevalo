93a9805bffff15609f12a746e917a4dd5d908763
Leobardo.arevalo25 <leobardo.arevalo25@cs104u-3c.acad.fullerton.edu>
2019-08-31 15:12:26 -0700

    finish prob 2

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -4,21 +4,23 @@
 
 int main()
 {
-  double hours, rate, pay;
+  double candy, rate, pay;
 
   // Get the number of hours worked.
-  std::cout << "How many hours did you work? ";
-  std::cin >> hours;
+  std::cout << "Welcome to the earnings calculator!/n";
 
   // Get the hourly pay rate.
-  std::cout << "How much do you get paid per hour? ";
+  std::cout<<"How many candy bars did you sell?";
+  std::cin >> candy;
+
+  std::cout<<"How much does the organization earn for each bar sold?";
   std::cin >> rate;
 
   // Calculate the pay.
-  pay = hours * rate;
+  pay = candy * rate;
 
   // Display the pay.
-  std::cout << "You have earned $" << pay << std::endl;
+  std::cout << "congragulations! You have earned $" << pay <<"for the organization"<< std::endl;
 
   return 0;
 }
